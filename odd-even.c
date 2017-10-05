a028ec02597b9cc9217967200d284163471a7bef
Fahim Shahriar <fahimshahriar143@gmail.com>
2017-10-05 23:58:18 +0600

    Add files via upload

---
+++ odd-even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if(n%2==1)
+    {
+        printf("The number is: Odd");
+
+    }
+    else{
+        printf("The number is: Even");
+    }
+    return 0;
+}
